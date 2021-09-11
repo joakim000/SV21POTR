06570c0ecc72c1c94014ef4612857474d6335b9a
joakim000 <joakim.odermalm@gmail.com>
2021-09-11 10:13:53 +0200

    work on void-method.-ö

--- Selfstudy/da_test.c
+++ Selfstudy/da_test.c
@@ -6,51 +6,65 @@
 #include <stdarg.h>
 #include "da.h"
 
-int main(){
+int main() {
     da test;
-    printf("Init exit:%d\n", daInit(&test, 5, DOUBLE));
+    
+    
+    printf("Init exit:%d\n", daInit(&test, 5, _double));
 
     double v[] = {0, 1, 2, 3, 4, 5, 6};
     //double v[] = {0, 1, 2, 3};
 
-    printf("\nCreate exit:%d\n", daCreate(&test, v, COUNT_OF(v)));
+    // printf("\nCreate exit:%d\n", daCreate(&test, v, COUNT_OF(v)));
     
     //printf("%f\n", daGet(&test, 2));
 
     int i;
+    double out;
+   /*  printf("Dump: ");
+    for (i = 0; i < test.elements; i++){
+        //printf("%f ", *(test.p+i));
+        daGet(&test, i, &out);
+        printf("%f ", out);
+    }
+    printf("\n"); */
+
+    double addVal = 10.123;
+    printf("Add exit:%d\n", daAdd(&test, 2, &addVal));
     printf("Dump: ");
     for (i = 0; i < test.elements; i++){
-        printf("%f ", *(test.p+i));
-        //printf("%f ", daGet(&test, i));
+        // printf("%f ", *(test.p+i));
+        // printf("%f ", daGet(&test, i));
+        daGet(&test, i, &out);
+        printf("%f ", out);
     }
     printf("\n");
 
-    printf("Add exit:%d\n", daAdd(&test, 2, 10.123));
+    addVal = 11.123;
+    printf("Add exit:%d\n", daAdd(&test, -1, &addVal));
     printf("Dump: ");
     for (i = 0; i < test.elements; i++){
-        printf("%f ", *(test.p+i));
+        // printf("%f ", *(test.p+i));
+        // printf("%f ", daGet(&test, i));
+        daGet(&test, i, &out);
+        printf("%f ", out);
     }
     printf("\n");
 
-    printf("Add exit:%d\n", daAdd(&test, -1, 11.123));
+
+    // printf("CompactRemove exit:%d\n", daCompactRemove(&test, 2, 2));
     printf("Dump: ");
     for (i = 0; i < test.elements; i++){
-        printf("%f ", *(test.p+i));
+        // printf("%f ", *(test.p+i));
+        // printf("%f ", daGet(&test, i));
     }
     printf("\n");
 
-
-    printf("CompactRemove exit:%d\n", daCompactRemove(&test, 2, 2));
+    // printf("CompactRemove exit:%d\n", daCompactRemove(&test, 2, 4));
     printf("Dump: ");
     for (i = 0; i < test.elements; i++){
-        printf("%f ", *(test.p+i));
-    }
-    printf("\n");
-
-    printf("CompactRemove exit:%d\n", daCompactRemove(&test, 2, 4));
-    printf("Dump: ");
-    for (i = 0; i < test.elements; i++){
-        printf("%f ", *(test.p+i));
+        // printf("%f ", *(test.p+i));
+        // printf("%f ", daGet(&test, i));
     }
     printf("\n");
 
