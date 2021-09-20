cb45800d4e83fe0e7ac0971a4591edf64bbbde32
Joakim <joakim.odermalm@gmail.com>
2021-09-20 13:24:10 +0200

    New subdir

---
+++ Tuesday exercises v37/devheaders.h
@@ -0,0 +1,21 @@
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+//#include <float.h>
+//#include <limits.h>
+#include <locale.h>
+#include <math.h>
+//#include <setjmp.h>
+//#include <signal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x]))))) //Google's clever array size macro
+
+// Timing
+clock_t start; clock_t end; float stopwatch;
+ 
