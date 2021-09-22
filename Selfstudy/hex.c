424bd9a0904a4e888bdf1d86763123261e1de469
Joakim <joakim.odermalm@gmail.com>
2021-09-22 12:02:54 +0200

    fun with hex

---
+++ Selfstudy/hex.c
@@ -0,0 +1,23 @@
+#include "devheaders.h"
+
+
+void main(){
+
+    for (int i = 0; i < 33; i++) {
+        // long long num = (long long)-pow(2,i); 
+        long long num = (long long)pow(2,i); 
+
+        
+        // printf("2^%-2d %#10o %10d %#10x\n", i, num, num, num);
+
+        // _printf_p("2^%1$-2d %2$#24llo %2$24llu %2$#24llx\n", i, num);
+
+        _printf_p("2^%1$-2d %2$#24llo %2$24lld %2$#24llx\n", i, num);
+        
+
+    }
+
+    // int x = 0x80, y = -0x80;
+        // _printf_p("%1$d %1$x %2$d %2$x\n", x, y);
+}
+
