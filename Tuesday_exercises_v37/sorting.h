c6711575a53cc45db69a4e1b5ed309586081e7f5
Joakim <joakim.odermalm@gmail.com>
2021-09-22 13:56:42 +0200

    Fixed bug in bubble sort

--- Tuesday_exercises_v37/sorting.h
+++ Tuesday_exercises_v37/sorting.h
@@ -35,7 +35,7 @@ void sort_bubble(uint32_t *num, uint8_t size)
     bool swapped; 
     do {
         swapped = false;
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size-1; i++) {
             if (num[i] > num[i + 1]) {
                 swap(&num[i], &num[i + 1]);
                 swapped = true;
