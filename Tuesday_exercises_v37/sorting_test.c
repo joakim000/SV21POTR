dedc4bfe558e35b4fc3098e8c019b35aad647326
Joakim <joakim.odermalm@gmail.com>
2021-09-29 14:04:07 +0200

    Compare func for debug / func test

--- Tuesday_exercises_v37/sorting_test.c
+++ Tuesday_exercises_v37/sorting_test.c
@@ -1,27 +1,27 @@
 #include "devheaders.h"
 #include "sorting.h"
 
-#define ELEMENTS 7    // max 259056
+#define ELEMENTS 20    // max 259056
 
 
 void generate_array(uint32_t *num, uint32_t size);
 void print_array(uint32_t *num, uint32_t size);
 void copy_array(uint32_t *num, uint32_t size, uint32_t *out);
 void compare_array(uint32_t *num, uint32_t size, uint32_t *comp);
-
+void compare_print_array(uint32_t *num, uint32_t size, uint32_t *comp);
 
 
 
 void main(){
     srand(time(0));
     uint32_t random[ELEMENTS] = {};
-    uint32_t numbers[ELEMENTS] = {};
+    // uint32_t numbers[ELEMENTS] = {};
 
     generate_array(random, ELEMENTS);
 
 
 
-    uint32_t num[] = {23, 1, 2, 43, 3, 34, 65, 5 };
+    // uint32_t num[] = {23, 1, 2, 43, 3, 34, 65, 5 };
     // uint32_t num[] = {1, 4, 5, 43, 3, 34, 45, 65 };
     // sort_merge(random, ELEMENTS);
     // printf("\n");
@@ -36,7 +36,7 @@ void main(){
     uint32_t mergeNums[ELEMENTS];
     copy_array(random, ELEMENTS, mergeNums);
     sort_merge(mergeNums, ELEMENTS);
-    compare_array(mergeNums, ELEMENTS, shellNums);
+    compare_print_array(mergeNums, ELEMENTS, shellNums);
 
 }
 
@@ -49,6 +49,17 @@ void generate_array(uint32_t *num, uint32_t size)
 }
 
 void compare_array(uint32_t *num, uint32_t size, uint32_t *comp)
+{
+    int errors = 0;
+    for (size_t i = 0; i < size; i++)
+    {
+        if (num[i] != comp[i])
+            errors++;
+    }
+    printf("\n%d errors in %d elements\n", errors, size);
+}
+
+void compare_print_array(uint32_t *num, uint32_t size, uint32_t *comp)
 {
     int errors = 0;
     printf("[");
@@ -81,7 +92,7 @@ void print_array(uint32_t *num, uint32_t size)
         if (i != 0 && (i + 1) % 10 == 0 && i + 1 != size)
             printf("\n");
     }
-    printf("]\n\n");
+    printf("]\n");
 }
 
 void copy_array(uint32_t *num, uint32_t size, uint32_t *out) {
