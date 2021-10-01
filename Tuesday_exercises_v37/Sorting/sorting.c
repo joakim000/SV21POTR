160e661cb2317e4eef6e1a6ad6cde411eb50dce3
Joakim <joakim.odermalm@gmail.com>
2021-10-01 22:05:25 +0200

    A little reorganization

--- Tuesday_exercises_v37/Sorting/sorting.c
+++ Tuesday_exercises_v37/Sorting/sorting.c
@@ -1,186 +1,369 @@
-/*  Sorting exercise
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+ 
+#include "sorting.h"
 
-    Main file: sorting.c
 
-    This is a companion running script with some helper functions,
-    prettyprint tables, functionality test and basic timing.
-    stdlib qsort used as reference.
+/* Tuesday exercises
+    ## Sorting
+    4. Sort an array of unsigned ints. Make two functions, one Ascending, one Descending.
+       To get you started: sorting.c
+
 */
 
-#include "sorting_lib.h"
+
+/*  Sorting exercise
+
+    This file contains natural language descriptions of a 
+	selection of well-known algorithms:
+        a. Bubble    - done
+		b. Insertion - done 
+		c. Selection - done
+        d. Shell     - done
+		e. Merge     - done
+        f. Quick
+*/
+
+
+/**
+ * ISSUES
+ * 
+ */
 
 // Flags
-#define PRT false // print random tables
-#define PT false  // print result tables
-#define CT true // qsort comparison testing
-#define T true // timing
+#define DEBUGCOPY false
+#define DEBUGWRITE false
+#define DEBUG false
 
-#define    BUBBLE false
-#define INSERTION true
-#define SELECTION true
-#define     SHELL true
-#define     MERGE  true
-#define     QUICK false
 
-// Sort size
-#define ELEMENTS 1000000   
+/* Lib qsort
+    Assumed error-free, use as comparison for functional tests
+    and as performance reference.
+*/
+int cmpfunc (const void * a, const void * b) {  // From tutorialspoint
+   return ( *(int*)a - *(int*)b );
+}
+void sort_lib(uint32_t *num, uint32_t size) {
+    qsort(num, size, sizeof(int), cmpfunc);
+}
 
-// Declarations
-void generate_array(uint32_t *num, uint32_t size);
-void generate_array_bigrand(uint32_t *num, uint32_t size, uint64_t ceil);
-void print_array(uint32_t *num, uint32_t size);
-void copy_array(uint32_t *num, uint32_t size, uint32_t *out);
-void compare_array(uint32_t *num, uint32_t size, uint32_t *comp);
 
-int main(void)
+
+
+/*  Bubble sort:
+    The algorithm starts at the beginning of the data set.
+    It compares the first two elements, and if the first is greater than the second, it swapped them.
+    It continues doing this for each pair of adjacent elements to the end of the data set.
+    It then starts again with the first two elements, repeating until no swapped have occurred on the last pass.
+*/
+
+void swap(uint32_t *a, uint32_t *b)
 {
-    srand(time(0));
-
-    uint32_t* random = calloc(ELEMENTS, sizeof(uint32_t));
-        assert( ("Memory allocation failed.", random != NULL) );
-    uint32_t* numbers = calloc(ELEMENTS, sizeof(uint32_t));
-        assert( ("Memory allocation failed.", numbers != NULL) );
-    uint32_t* compare = calloc(ELEMENTS, sizeof(uint32_t));
-        assert( ("Memory allocation failed.", compare != NULL) );
-
-    generate_array(random, ELEMENTS);
-    // generate_array_bigrand(random, ELEMENTS, UINT32_MAX);
-  
-    /* Lib */
-    copy_array(random, ELEMENTS, compare);
-    printf("\nLib qsort: \n"); 
-    timer_start = clock();
-        sort_lib(compare, ELEMENTS);
-    timer_end = clock();
-    if (PT) print_array(compare, ELEMENTS);
-    if (T) _printf_p("%d elements in %5.3f seconds.\n", ELEMENTS, TIMING(timer_start, timer_end));
-
-
-    /* Bubble */
-    if (BUBBLE) {
-        copy_array(random, ELEMENTS, numbers);
-        printf("\nBubble sort: \n"); 
-        timer_start = clock();
-            sort_bubble(numbers, ELEMENTS);
-        timer_end = clock();
-        if (PT) print_array(numbers, ELEMENTS);
-        if (T) _printf_p("%d elements in %5.3f seconds.\n", ELEMENTS, TIMING(timer_start, timer_end));
-        if (CT) compare_array(numbers, ELEMENTS, compare);
-    }
-
-    /* Insertion */
-    if (INSERTION) {
-        copy_array(random, ELEMENTS, numbers);
-        printf("\nInsertion sort: \n"); //Low to High
-        timer_start = clock();
-            sort_insertion(numbers, ELEMENTS);
-        timer_end = clock();
-        if (PT) print_array(numbers, ELEMENTS);
-        if (T) _printf_p("%d elements in %5.3f seconds.\n", ELEMENTS, TIMING(timer_start, timer_end));
-        if (CT) compare_array(numbers, ELEMENTS, compare);
-    }
-
-    /* Selection */
-    if (SELECTION) {
-        copy_array(random, ELEMENTS, numbers);
-        printf("\nSelection sort: \n"); //Low to High
-        timer_start = clock();
-            sort_selection(numbers, ELEMENTS);
-        timer_end = clock();
-        if (PT) print_array(numbers, ELEMENTS);
-        if (T) _printf_p("%d elements in %5.3f seconds.\n", ELEMENTS, TIMING(timer_start, timer_end));
-        if (CT) compare_array(numbers, ELEMENTS, compare);
-    }
-   
-    /* Shell */
-    if (SHELL) {
-        copy_array(random, ELEMENTS, numbers);
-        printf("\nShell sort: \n"); //Low to High
-        timer_start = clock();
-            sort_shell(numbers, ELEMENTS);
-        timer_end = clock();
-        if (PT) print_array(numbers, ELEMENTS);
-        if (T) _printf_p("%d elements in %5.3f seconds.\n", ELEMENTS, TIMING(timer_start, timer_end));
-        if (CT) compare_array(numbers, ELEMENTS, compare);
-    }
-   
-    /* Merge */
-    if (MERGE) {
-        copy_array(random, ELEMENTS, numbers);
-        printf("\nMerge sort: \n"); //Low to High
-        timer_start = clock();
-            sort_merge(numbers, ELEMENTS);
-        timer_end = clock();
-        if (PT) print_array(numbers, ELEMENTS);
-        if (T) _printf_p("%d elements in %5.3f seconds.\n", ELEMENTS, TIMING(timer_start, timer_end));
-        if (CT) compare_array(numbers, ELEMENTS, compare);
-    }
-
-    /* Quick */
-    if (QUICK) {
-        copy_array(random, ELEMENTS, numbers);
-        printf("\nQuick sort: \n"); //Low to High
-        timer_start = clock();
-            sort_quick(numbers, ELEMENTS);
-        timer_end = clock();
-        if (PT) print_array(numbers, ELEMENTS);
-        if (T) _printf_p("%d elements in %5.3f seconds.\n", ELEMENTS, TIMING(timer_start, timer_end));
-        if (CT) compare_array(numbers, ELEMENTS, compare);
-    }
-
-    free(random);
-    free(numbers);
-    free(compare);
-
-    return 0;
+    uint32_t tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
-/* Helper functions */
-void generate_array(uint32_t *num, uint32_t size)
+void sort_bubble(uint32_t *num, uint32_t size)
 {
-    for (size_t i = 0; i < size; i++)
-    {
-        num[i] = rand();
-    }
+    bool swapped; 
+    do {
+        swapped = false;
+        for (int i = 0; i < size-1; i++) {
+            if (num[i] > num[i + 1]) {
+                swap(&num[i], &num[i + 1]);
+                swapped = true;
+            }
+        }
+    } while (swapped == true);
 }
 
-void generate_array_bigrand(uint32_t *num, uint32_t size, uint64_t ceil)
+void sort_bubble_d(uint32_t *num, uint32_t size)
 {
-    for (size_t i = 0; i < size; i++)
-    {
-        num[i] = ((rand() << 16) | rand()) % ceil;
+    bool swapped; 
+    do {
+        swapped = false;
+        for (int i = 0; i < size-1; i++) {
+            if (num[i] < num[i + 1]) {
+               swap(&num[i], &num[i + 1]);
+                swapped = true;
+            }
+        }
+    } while (swapped == true);
+}
+
+
+
+
+/*  Insertion sort:
+ 1. Suppose there exists a function called Insert designed to insert a value into a sorted sequence at the beginning of an array.
+    It operates by beginning at the end of the sequence and shifting each element one place to the right until a suitable position is found for the new element.
+    The function has the side effect of overwriting the value stored immediately after the sorted sequence in the array.
+ 2. To perform an insertion sort, begin at the left-most element of the array and invoke Insert to insert each element encountered into its correct position.
+    The ordered sequence into which the element is inserted is stored at the beginning of the array in the set of indices already examined.
+    Each insertion overwrites a single value: the value being inserted.
+*/
+
+void insert(int index, uint32_t *num, uint32_t size, int direction ) {
+    int moveToIndex = index;
+    //Find place
+    if (direction == 1) 
+        for (int i = index - 1; i >= 0; i--) 
+            if (num[i] < num[index])
+                moveToIndex--;
+            else
+                break;
+    else 
+        for (int i = index - 1; i >= 0; i--) 
+            if (num[i] > num[index])
+                moveToIndex--;
+            else
+                break;
+
+    // Move elements up and insert value
+    if (moveToIndex != index) {
+        uint32_t tmp = num[index];
+        for (int i = index; i >= moveToIndex; i--) 
+            num[i] = num[i - 1];
+        num[moveToIndex] = tmp;
     }
 }
 
 
-void print_array(uint32_t *num, uint32_t size)
+void sort_insertion(uint32_t *num, uint32_t size)
 {
-    // printf("[");
-    for (size_t i = 0; i < size; i++)
-    {
-        printf("%12d", num[i]);
-        if (i < size - 1)
-            // printf(", ");
-        if (i != 0 && (i + 1) % 10 == 0 && i + 1 != size)
-            printf("\n");
-    }
-    printf("\n");
+    for (int i = 1; i < size; i++)
+        insert(i, num, size, 0);
 }
 
-void copy_array(uint32_t *num, uint32_t size, uint32_t *out) {
-    for (int i = 0; i < size; i++) 
-        out[i] = num[i];
-    if (PRT) printf("\nRandomized array (copy):\n");
-    if (PRT) print_array(out, ELEMENTS);
-}
-
-void compare_array(uint32_t *num, uint32_t size, uint32_t *comp)
+void sort_insertion_d(uint32_t *num, uint32_t size)
 {
-    int errors = 0;
-    for (size_t i = 0; i < size; i++)
-    {
-        if (num[i] != comp[i])
-            errors++;
-    }
-    printf("%d errors in %d elements.\n", errors, size);
+    for (int i = 1; i < size; i++)
+        insert(i, num, size, 1);
 }
+
+
+
+
+
+/*  Selection sort:
+    The algorithm divides the input list into two parts: 
+        a sorted sublist of items which is built up from left to right at the front (left) of the list
+        and a sublist of the remaining unsorted items that occupy the rest of the list.
+    Initially, the sorted sublist is empty and the unsorted sublist is the entire input list.
+    The algorithm proceeds by finding the smallest (or largest, depending on sorting order) element in the unsorted sublist,
+        exchanging (swapping) it with the leftmost unsorted element (putting it in sorted order),
+        and moving the sublist boundaries one element to the right.
+*/
+int select(int bound, uint32_t *num, uint32_t size, int small0large1) {
+    int r = bound;
+    if (small0large1 == 1) 
+        for (int i = bound + 1; i < size; i++) 
+            r = num[i] > num[r] ? i : r;        // select largest value
+    else
+        for (int i = bound + 1; i < size; i++) 
+            r = num[i] < num[r] ? i : r;        // select smallest value
+    return r;
+}
+
+void sort_selection(uint32_t *num, uint32_t size)
+{
+    for (int bound = 0; bound < size; bound++) {
+        int sel = select(bound, num, size, 0);
+        swap(&num[bound], &num[sel]);
+    }
+}
+
+void sort_selection_d(uint32_t *num, uint32_t size)
+{
+    for (int bound = 0; bound < size; bound++) {
+        int sel = select(bound, num, size, 1);
+        swap(&num[bound], &num[sel]);
+    }
+}
+
+
+
+
+/*  Shellsort:
+    Shellsort is an optimization of insertion sort that allows the exchange of items that are far apart.
+    The idea is to arrange the list of elements so that, starting anywhere, taking every hth element produces a sorted list.
+    Such a list is said to be h-sorted. 
+    It can also be thought of as h interleaved lists, each individually sorted. 
+    Beginning with large values of h allows elements to move long distances in the original list, reducing large amounts of disorder quickly,
+    and leaving less work for smaller h-sort steps to do. 
+    If the list is then k-sorted for some smaller integer k, then the list remains h-sorted.
+    Following this idea for a decreasing sequence of h values ending in 1 is guaranteed to leave a sorted list in the end.
+
+    gaps = [1750, 701, 301, 132, 57, 23, 10, 4, 1]  // Extended Ciura gap sequence
+                                                    // https://oeis.org/A102549/internal 
+*/
+void gapInsert(int index, int gap, uint32_t *num, uint32_t size, int direction ) {
+    int moveToIndex = index;
+    //Find place
+    if (direction == 1) 
+        for (int i = index - 1; i >= 0; i--) 
+            if (num[i] < num[index])
+                moveToIndex--;
+            else
+                break;
+    else 
+        for (int i = index - gap; i >= 0; i -= gap) 
+            if (num[i] > num[index])
+                moveToIndex -= gap;
+            else
+                break;
+
+    // Move elements up and insert value
+    if (moveToIndex != index) {
+        uint32_t tmp = num[index];
+        for (int i = index; i >= moveToIndex; i--) 
+            num[i] = num[i - 1];
+        num[moveToIndex] = tmp;
+    }
+}
+
+void sort_shell(uint32_t *num, uint32_t size)
+{
+    int gaps[] = {1750, 701, 301, 132, 57, 23, 10, 4, 1};  // Extended Ciura gap sequence
+    // int gaps[] = {4, 1};  
+    // int gaps[] = {5, 3, 1};
+    // int gaps[] = {3, 1};
+    // int gaps[] = {1};
+
+    for (int si = 0; si < COUNT_OF(gaps); si++) { // si: shell iterator
+        for (int i = 1; i < size; i++)
+            gapInsert(i, gaps[si], num, size, 0);
+    }   
+}
+
+
+
+
+
+
+/*  Merge sort:
+    Conceptually, a merge sort works as follows:
+    Divide the unsorted list into n sublists, each containing one element (a list of one element is considered sorted).
+    Repeatedly merge sublists to produce new sorted sublists until there is only one sublist remaining. This will be the sorted list.
+    Note: Top-down vs. bottom-up implementation.
+*/
+uint32_t totalSize; // DEBUG
+uint32_t* randomArray; // DEBUG
+
+// DEBUG helper
+bool inArray(uint32_t find, uint32_t *num, uint32_t size) {
+    for (int i = 0; i < size; i++) {
+       if (find == num[i]) 
+          return true;
+    }
+    return false;
+}
+
+// For holding temp values 
+uint32_t* a;
+uint32_t* b;
+
+void merge_recurse(uint32_t *num, uint32_t size, uint32_t start ) {
+    // Shouldn't happen
+    assert( ("Illegal array size", size > 0) );
+
+    // Split into 2 virtual parts    
+    int aSize = size / 2;
+    int bSize = size / 2 + size % 2;
+ 
+    // Continue splitting    
+    if (size > 2) {
+        merge_recurse(num, aSize, start);
+        merge_recurse(num, bSize, start + aSize);
+    }
+
+    // Now we're on the way up. First ignore size 1 (by definition sorted). Then start comparing.
+    if (size > 1) {
+
+        /* Copy values to be sorted for this call. */
+        // Begin reading a-values at start index for this call
+        for (int i = 0; i < aSize; i++) { 
+            a[i] = num[start + i];
+        }
+
+        // Read b-values at count of a-values further along array 
+        for (int i = 0; i < bSize; i++) { 
+            b[i] = num[start + aSize + i];
+        }
+
+        /* Compare and write values into sort array. */
+        int p = start; // For this call, start writing here. Later increment p whenever writing value.
+
+        for (int i = 0, j = 0; i < aSize; i++) {
+            // Case 1: have more a-values but out of b-values. Continue looping over and writing a-vals.
+            //    Forgetting about this was source of intermittent errors depending on look of random array. 
+            // Case 2: a-val is already < b-val, write and continue to next a-val
+            if (j >= bSize || a[i] < b[j]) {
+                num[p++] = a[i];
+                continue;
+            }
+            
+            // Case 3: b-val < a-val. Continue writing b-vals until current a-val is smaller or b-vals exhausted.
+            do {
+                num[p++] = b[j++];
+            }  while (j < bSize && b[j] < a[i]);
+            // After 1 or more b-vals, finally write current a-val. 
+            num[p++] = a[i];
+        }
+    }
+}
+
+void sort_merge(uint32_t *num, uint32_t size)
+// void sort_merge(uint32_t *num, uint32_t size, uint32_t *random)  //DEBUG
+{
+    // To hold temp values
+    uint32_t* aTemp;
+    aTemp = calloc(size / 2 + 2, sizeof(uint32_t));
+    assert( ("Memory allocation failed.", aTemp != NULL) );
+    a = aTemp;
+
+    uint32_t* bTemp;
+    bTemp = calloc(size / 2 + 2, sizeof(uint32_t));
+    assert( ("Memory allocation failed.", bTemp != NULL) );
+    b = bTemp;
+
+    // totalSize = size; // DEBUG
+    // randomArray = random; // DEBUG
+
+    merge_recurse(num, size, 0);
+    free(aTemp);
+    free(bTemp);
+}
+
+
+
+
+
+
+/*  Quicksort:
+ 1. If the range has less than two elements, return immediately as there is nothing to do. 
+    Possibly for other very short lengths a special-purpose sorting method is applied and the remainder of these steps skipped.
+ 2. Otherwise pick a value, called a pivot, that occurs in the range (the precise manner of choosing depends on the partition routine, and can involve randomness).
+ 3. Partition the range: reorder its elements, while determining a point of division, 
+    so that all elements with values less than the pivot come before the division,
+    while all elements with values greater than the pivot come after it;
+    elements that are equal to the pivot can go either way.
+    Since at least one instance of the pivot is present, most partition routines ensure that the value that ends up at the point of division is equal to the pivot,
+    and is now in its final position (but termination of quicksort does not depend on this, as long as sub-ranges strictly smaller than the original are produced).
+ 4. Recursively apply the quicksort to the sub-range up to the point of division and to the sub-range after it, 
+    possibly excluding from both ranges the element equal to the pivot at the point of division.
+    (If the partition produces a possibly larger sub-range near the boundary where all elements are known to be equal to the pivot, these can be excluded as well.)
+*/
+void sort_quick(uint32_t *num, uint32_t size)
+{
+    //implement me!
+}
+
+
+
+
+
