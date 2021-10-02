b08ba4e4d8880b2c8a27d9ecd9dd227cc640c9b8
Joakim <joakim.odermalm@gmail.com>
2021-10-02 16:49:17 +0200

    cross-plattform

--- Tuesday_exercises_v37/Sorting/sorting.h
+++ Tuesday_exercises_v37/Sorting/sorting.h
@@ -8,9 +8,7 @@
 
 #define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x]))))) //Google's clever array size macro
 
-// Timing
-#define TIMING(y, x) ((float)(x - y) / CLOCKS_PER_SEC)
-clock_t timer_start; clock_t timer_end; 
+
 
 void sort_lib(uint32_t *num, uint32_t size);
 void sort_lib_d(uint32_t *num, uint32_t size);
