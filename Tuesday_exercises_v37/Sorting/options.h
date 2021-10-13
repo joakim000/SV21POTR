640de6576c887a2a8f8ad3f36eacc6ccd671e6d1
Joakim <joakim.odermalm@gmail.com>
2021-10-13 21:28:04 +0200

    changed -test to -notest

--- Tuesday_exercises_v37/Sorting/options.h
+++ Tuesday_exercises_v37/Sorting/options.h
@@ -3,7 +3,7 @@
 // Flags    true: disregard arg, always run
 #define PRT false  // print input tables        -it
 #define PT false  // print result tables        -rt
-#define CT true  // qsort comparison testing    -test
+#define CT true  // qsort comparison testing    -notest
 #define T false // timing                       -time
 
 // Printing sanity check
@@ -20,8 +20,8 @@
 
 /* Sort set options */
 // Size                                         -size=X
-//#define ELEMENTS 100
-#define ELEMENTS 5000000                        
+#define ELEMENTS 100
+// #define ELEMENTS 5000000                        
 
 // Max random value                             -max={i8, i16, i32}
 //#define RND_MAX INT16_MAX
@@ -33,3 +33,36 @@
 
 // Debugging
 #define DEBUG true
+
+
+// Help text
+char* HELPTEXT1 = "\
+ Flags\n \
+  print input tables         -it\n \
+  print result tables        -rt\n \
+  disable testing            -notest\n \
+  timing                     -time\n \
+\n \
+Sorts to run\n \
+    BUBBLE   bub\n \
+ INSERTION   ins\n \
+ SELECTION   sel\n \
+     SHELL   she\n \
+     MERGE   mer\n \
+     QUICK   qui\n \
+Default: \
+";
+
+char* HELPTEXT2 = "\
+\n\n \
+Sort set\n \
+  Size                         [100]   -size value\n \
+  Biggest random value         [i32]   -max {i8, i16, i32, value}\n \
+  Composition                  [1]     -run value\n \
+    0: all linear\n \
+    1: all random\n \
+    >1: alternating runs\n \
+        of value length.\n \
+\n \
+Print size (sanity check)      [100]   -tmax value\n \
+";
