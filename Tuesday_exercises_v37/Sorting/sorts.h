8c8519012b2059f9c7896fa8590d5e889db51c48
Joakim <joakim.odermalm@gmail.com>
2021-10-20 01:29:03 +0200

    Moved 2 structs here

--- Tuesday_exercises_v37/Sorting/sorts.h
+++ Tuesday_exercises_v37/Sorting/sorts.h
@@ -1,30 +1,41 @@
-/* Sorts for sorting exercise run -           
+/* Sorts for sorting exercise bench -           
         edit this file to add or remove sorts
 */
 
 #include <stdint.h>
+#include "options.h"
+#define SORTS_H
+
+typedef struct sort_holder {
+    char* name;
+    char* print_name;
+    bool default_run;
+    // void (*sort_ptr)(uint32_t*, uint32_t size); // ptr to sort function
+    void (*sort_ptr)(void* args); // ptr to sort function
+    bool run;
+} sort;
 
 // Declare sort functions implemented in sorting.c
-void sort_lib(uint32_t *num, uint32_t size);
-void sort_bubble(uint32_t *num, uint32_t size);
-void sort_insertion(uint32_t *num, uint32_t size);
-void sort_selection(uint32_t *num, uint32_t size);
-void sort_shell(uint32_t *num, uint32_t size);
-void sort_merge(uint32_t *num, uint32_t size);
-void sort_tmerge(uint32_t *num, uint32_t size);
-void sort_quick(uint32_t *num, uint32_t size);
-void sort_radix(uint32_t *num, uint32_t size);
+void sort_lib(void *params);
+void sort_bubble(void *params);
+void sort_insertion(void *params);
+void sort_selection(void *params);  
+void sort_shell(void *params);
+void sort_merge(void *params);
+void sort_tmerge(void *params);
+void sort_quick(void *params);
+void sort_radix(void *params);
 
 // Descending variants
-void sort_lib_d(uint32_t *num, uint32_t size);
-void sort_bubble_d(uint32_t *num, uint32_t size);
-void sort_insertion_d(uint32_t *num, uint32_t size);
-void sort_selection_d(uint32_t *num, uint32_t size);
-void sort_shell_d(uint32_t *num, uint32_t size);
-void sort_merge_d(uint32_t *num, uint32_t size);
-void sort_tmerge_d(uint32_t *num, uint32_t size);
-void sort_quick_d(uint32_t *num, uint32_t size);
-void sort_radix_d(uint32_t *num, uint32_t size);
+void sort_lib_d(void *params);
+void sort_bubble_d(void *params);
+void sort_insertion_d(void *params);
+void sort_selection_d(void *params);
+void sort_shell_d(void *params);
+void sort_merge_d(void *params);
+void sort_tmerge_d(void *params);
+void sort_quick_d(void *params);
+void sort_radix_d(void *params);
 
 
 // Init sorts, using default run setting from options.h
@@ -37,8 +48,8 @@ sort sorts_a[NUMBER_OF_SORTS] = {
     {.name="sel", .print_name="Selection sort", .default_run=SELECTION, .sort_ptr = sort_selection},
     {.name="she", .print_name="Shell sort", .default_run=SHELL, .sort_ptr = sort_shell},
     {.name="mer", .print_name="Merge sort", .default_run=MERGE, .sort_ptr = sort_merge},
-    {.name="qui", .print_name="Quick sort", .default_run=QUICK, .sort_ptr = sort_quick},
     {.name="tme", .print_name="Merge sort (threaded)", .default_run=TMERGE, .sort_ptr = sort_tmerge},
+    {.name="qui", .print_name="Quick sort", .default_run=QUICK, .sort_ptr = sort_quick},
     {.name="rad", .print_name="Radix sort", .default_run=RADIX, .sort_ptr = sort_radix}
 }; 
 
@@ -49,9 +60,18 @@ sort sorts_d[NUMBER_OF_SORTS] = {
     {.name="sel", .print_name="Selection sort", .default_run=SELECTION, .sort_ptr = sort_selection_d},
     {.name="she", .print_name="Shell sort", .default_run=SHELL, .sort_ptr = sort_shell_d},
     {.name="mer", .print_name="Merge sort", .default_run=MERGE, .sort_ptr = sort_merge_d},
-    {.name="qui", .print_name="Quick sort", .default_run=QUICK, .sort_ptr = sort_quick_d},
     {.name="tme", .print_name="Merge sort (threaded)", .default_run=TMERGE, .sort_ptr = sort_tmerge_d},
+    {.name="qui", .print_name="Quick sort", .default_run=QUICK, .sort_ptr = sort_quick_d},
     {.name="rad", .print_name="Radix sort", .default_run=RADIX, .sort_ptr = sort_radix_d}
 }; 
 
 
+
+struct sort_args {
+    // Standard args
+    uint32_t* num;
+    uint32_t size;
+    
+    uint16_t maxThreads; // For threaded sorts 
+    int shellSeq[]; // Gap sequence for shell sort
+};
