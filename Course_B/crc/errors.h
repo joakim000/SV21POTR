056361534594ce68fa92293ee395e7bf9cbd3eba
Joakim <joakim.odermalm@gmail.com>
2021-10-31 21:18:09 +0100

    Better constant name

--- Course_B/crc/errors.h
+++ Course_B/crc/errors.h
@@ -1,6 +1,6 @@
 /* Make errors to match assignment examples
     Input          Example result  Spec result
-    123456789      0x59E           0x2CF
+    "123456789"    0x67F5*         0x5136
     "AB"           0x54FB          0x48B1
     "ABC"          0xD59           0x6460
     "ABCD"         0x6531          0x5054
@@ -9,9 +9,9 @@
     "Faroch"       0x2535          0x7056
 */
 #if defined(MATCH_EXAMPLES)
-#define PADSIZE (*crc).n + 1             // Overpad by 1 bit
-#define REMLOOPEND msgSize - (*crc).n    // Allow processing to continue after message end, 1 step / overpadding bit.
+#define SPECIALWIDTH crc->n + 1             // Overpad by 1 bit
+#define REMLOOPEND msgSize - crc->n    // Allow processing to continue after message end, 1 step / overpadding bit.
 #else
-#define PADSIZE (*crc).n                 // Normal padding
+#define SPECIALWIDTH crc->n                 // Normal padding
 #define REMLOOPEND originalMsgSize       // Always stop processing at message end. This mitigates errors from overpadding.
 #endif
